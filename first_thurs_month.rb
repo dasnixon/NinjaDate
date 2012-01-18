fb1d374b9d05e9883e20e52eb534a951f1af7967
Chris Nixon <nixoncd@mail.uc.edu>
2012-01-17 23:52:21 -0500

    Added comments to file for understanding the steps.

--- first_thurs_month.rb
+++ first_thurs_month.rb
@@ -18,7 +18,12 @@ end
 
 class NinjaDate
   def self.get_first_thurs_month
-  next_month = Chronic.parse("next month")
+  next_month = Chronic.parse("next month") #chronic gem nicely gets us the date for the next month
+  #Use strftime on the date object returned from chronic to get the day of the month and convert
+  #to an integer. Pass that int to built in monthnames constant. Once we get back the name of
+  #the month we pass that to chronic gem again to get the first thursday in that month. Then use
+  #strftime to properly print out the date and use the Fixnum class ordinalize method for the day.
+  #Finally print out to stdout.
   first_thurs_next_month = Chronic.parse("1st thursday in #{Date::MONTHNAMES[next_month.strftime("%m").to_i]}")
   exact_day = first_thurs_next_month.strftime("%A, %B #{first_thurs_next_month.day.ordinalize}, %Y")
   puts "#{exact_day}"
